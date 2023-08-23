c5bcfcf8bf3a0d3430a08898b617c4e168fb5591
sahdev grover <sahdevgrover07@gmail.com>
2023-08-23 18:55:15 +0530

    Add Day 26 terraform instructions

--- 26_Day26/provisioning.tf
+++ 26_Day26/provisioning.tf
@@ -10,3 +10,10 @@ module "ec2-web" {
   source    = "ec2"         # path to your module directory
   ec2_sg_id = module.sg_module.sg-id   # module.<module name>.<output name>
 }
+
+output "ec2_sg_id" {
+  value = module.sg_module.sg-id # module.<module name>.<output name>
+}
+output "ec2_instance_id" {
+  value = module.ec2-web.ec2_instance_id
+}
