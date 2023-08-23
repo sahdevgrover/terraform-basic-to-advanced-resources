1ebe1ffc1ddff071592755e66698ec45aabab98f
sahdev grover <sahdevgrover07@gmail.com>
2023-08-23 19:03:52 +0530

    Add Day 26 terraform instructions

--- 26_Day26/ec2/ec2.tf
+++ 26_Day26/ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {               # creating ec2 instance
-  ami           = "ami-0ed752ea0f62749af"     # Specify your AMI ID as per the region
+  ami           = "ami-0da59f1af71ea4ad2"     # Specify your AMI ID as per the region
   instance_type = "t2.large"                  # instance_type
   vpc_security_group_ids = [var.ec2_sg_id]    # reference to the variable which holds the id of sg
 }
--- 26_Day26/provisioning.tf
+++ 26_Day26/provisioning.tf
@@ -1,13 +1,13 @@
 provider "aws" {               # provider block (aws)
-  region = "eu-west-1"         # specify the region in which you would like to create your resources.
+  region = "ap-south-1"         # specify the region in which you would like to create your resources.
 }
 
 module "sg_module" {
-  source = "sg"             # path to your module directory
+  source = "./sg"             # path to your module directory
 }
 
 module "ec2-web" {
-  source    = "ec2"         # path to your module directory
+  source    = "./ec2"         # path to your module directory
   ec2_sg_id = module.sg_module.sg-id   # module.<module name>.<output name>
 }
 
